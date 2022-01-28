f49548bb8b10f28a05aa69e88b7079c49115f1cd
Mat <60005715+MatWasTaken@users.noreply.github.com>
2022-01-28 14:15:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-apt-get install golang-any clamav clamav-freshclam clamav-daemon clamav-libunrar
+apt-get install golang-any clamav clamav-freshclam clamav-daemon
 
-sed -i 's/^#Foreground .*$/Foreground true/g' /etc/clamav/clamd.conf
-sed -i 's/^#TCPSocket .*$/TCPSocket 3310/g' /etc/clamav/clamd.conf 
-sed -i 's/^#Foreground .*$/Foreground true/g' /etc/clamav/freshclam.conf
+cp ./clamrest /usr/bin/clamav-rest
+
+sed -i 's/^Foreground .*$/Foreground true/g' /etc/clamav/clamd.conf
+sed -i '/^#Please/a TCPSocket 3310' /etc/clamav/clamd.conf
+sed -i 's/^Foreground .*$/Foreground true/g' /etc/clamav/freshclam.conf
+
+service clamav-daemon stop
+service clamav-freshclam stop
 freshclam --quiet
+service clamav-daemon start
+service clamav-freshclam start
 
-iptables -A INPUT -p tcp --dport 9000 -j ACCEPT
-
-mv ./clamrest /usr/bin/
 mv ./service/entrypointClamAV.sh /usr/bin/
 chmod +x /usr/bin/entrypointClamAV.sh
 mv ./service/entrypointClamAV.service /usr/lib/systemd/system/
 
-systemctl daemon-restart
+systemctl daemon-reload
 systemctl enable entrypointClamAV.service
 systemctl start entrypointClamAV.service
-
